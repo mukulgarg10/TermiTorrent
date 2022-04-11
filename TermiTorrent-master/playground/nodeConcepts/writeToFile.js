070dc43bf74faf99e5cfc53525bfd3c5966619f9
mukulgarg10 <83575819+mukulgarg10@users.noreply.github.com>
2022-04-11 19:53:34 +0530

    Delete TermiTorrent-master directory

--- TermiTorrent-master/playground/nodeConcepts/writeToFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const fd = fs.openSync('./temp.txt', 'w');
-fs.write(fd, Buffer.from('Hello_1'), 0, 7, 10, ()=>{});
-fs.write(fd, Buffer.from('Hello_2'), 0, 7, 20, ()=>{});
-fs.close(fd, ()=>{});
